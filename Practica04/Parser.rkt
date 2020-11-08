4ed3236953a25dccef3183a6204049b42f044330
César Jardines <cesarjardines@MacBook-Pro-de-Cesar.local>
2020-11-08 17:59:46 -0600

    Nueva gramatica añadida

--- Practica04/Parser.rkt
+++ Practica04/Parser.rkt
@@ -14,6 +14,8 @@ Lexer y parser
          parser-tools/yacc
          parser-tools/lex)
 
+(provide (all-defined-out))
+
 ;; An abstraction of the grammar minHS.
 (define-struct var-exp (i) #:transparent)   ; For the variables.
 (define-struct num-exp (n) #:transparent)   ; For the numbers.
@@ -44,7 +46,7 @@ Lexer y parser
 
 ;Se agrega define para begin
 (define-struct begin-exp (exp) #:transparent)
-(define-struct if-then-exp (g e1)#:transparent)
+(define-struct if-then-exp1 (g e1)#:transparent)
 
 (define minHS-parser
   (parser
@@ -73,7 +75,7 @@ Lexer y parser
 
      ;Se agrega begin e if con then 
      ((BEGIN exp) (begin-exp $2))
-     ((IF LP exp RP THEN LK exp RK) (if-then-exp $3 $7))
+     ((IF LP exp RP THEN LK exp RK) (if-then-exp1 $3 $7))
 
      
      ((IF LP exp RP THEN LK exp RK ELSE LK exp RK) (if-then-exp $3 $7 $11));
@@ -109,65 +111,75 @@ Lexer y parser
 (define (lex-this lexer input) (lambda () (lexer input)))
 
 ;A lot of examples.
+#|
 (display "Example 1: 3 - (3 / 6)\n")
 (let ((input (open-input-string "3 - (3 / 6)")))
   (minHS-parser (lex-this minHS-lexer input)))
+|#
 #|
 Desired response:
 (prim-exp #<procedure:-> (num-exp 3) (par-exp (prim-exp #<procedure:/> (num-exp 3) (num-exp 6))))
 |#
-
+#|
 (display "\nExample 2: if(#t)then{2}else{3}\n")
 (let ((input (open-input-string "if(#t)then{2}else{3}")))
   (minHS-parser (lex-this minHS-lexer input)))
+|#
 #|
 Desired response:
 ;; (if-exp (bool-exp #t) (num-exp 2) (num-exp 3))
 |#
-
+#|
 (display "\nExample 3: fun ([x:Int]:Int) => x\n")
 (let ((input (open-input-string "fun ([x:Int]:Int) => x")))
   (minHS-parser (lex-this minHS-lexer input)))
+|#
 #|
 Desired response:
 (fun-exp (typeof-exp (brack-exp (typeof-exp (var-exp 'x) (int-exp))) (int-exp)) (var-exp 'x))
 |#
-
+#|
 (display "\nExample 4: fun ([f:Func Int Int]:Int) => f app 1\n")
 (let ((input (open-input-string "fun ([f:Func Int Int]:Int) => f app 1")))
   (minHS-parser (lex-this minHS-lexer input)))
+|#
 #|
 Desired response:
 (fun-exp (typeof-exp (brack-exp (typeof-exp (var-exp 'f) (func-exp (int-exp) (int-exp)))) (int-exp)) (app-exp (var-exp 'f) (num-exp 1)))
 |#
-
+#|
 (display "\nExample 5: fun ([f:Func (Func Int Bool) Int]:Bool) => #t\n")
 (let ((input (open-input-string "fun ([f:Func (Func Int Bool) Int]:Bool) => #t")))
   (minHS-parser (lex-this minHS-lexer input)))
+|#
 #|
 Desired response:
 (fun-exp (typeof-exp (brack-exp (typeof-exp (var-exp 'f) (func-exp (par-exp (func-exp (int-exp) (boole-exp))) (int-exp)))) (boole-exp)) (bool-exp #t))
 |#
-
+#|
 (display "\nExample 6: funF (sumita ([x:Int][y:Int]):Int) => x+y\n")
 (let ((input (open-input-string "funF (sumita ([x:Int][y:Int]):Int) => x+y")))
   (minHS-parser (lex-this minHS-lexer input)))
+|#
 #|
 Desired response:
 (fun-f-exp (typeof-exp (var-exp 'sumita) (brack-exp (app-t-exp (typeof-exp (var-exp 'x) (int-exp)) (typeof-exp (var-exp 'y) (int-exp))))) (int-exp) (prim-exp #<procedure:+> (var-exp 'x) (var-exp 'y)))
 |#
-
+#|
 (display "\nExample 7: let ([x:Int = 1][y:Int = 2]) in x+y end\n")
 (let ((input (open-input-string "let ([x:Int = 2][y:Int = 2]) in x+y end")))
   (minHS-parser (lex-this minHS-lexer input)))
+|#
 #|
 Desired response:
 (let-exp (brack-exp (app-t-exp (assign-exp (typeof-exp (var-exp 'x) (int-exp)) (num-exp 1)) (assign-exp (typeof-exp (var-exp 'y) (int-exp)) (num-exp 2)))) (prim-exp #<procedure:+> (var-exp 'x) (var-exp 'y)))
 |#
 
+#|
 (display "\nExample 8: ((funF (sumita ([x:Int][y:Int]):Int) => x+y) app 2) app 4\n")
 (let ((input (open-input-string "((funF (sumita ([x:Int][y:Int]):Int) => x+y) app 2) app 4")))
-  (minHS-parser (lex-this minHS-lexer input)))
+|#
+ ; (minHS-parser (lex-this minHS-lexer input)))
 #|
 Desired response:
 (app-exp(par-exp(app-exp(par-exp(fun-f-exp(typeof-exp (var-exp 'sumita) (brack-exp (app-t-exp (typeof-exp (var-exp 'x) (int-exp)) (typeof-exp (var-exp 'y) (int-exp)))))(int-exp)(prim-exp #<procedure:+> (var-exp 'x) (var-exp 'y))))(num-exp 2)))(num-exp 4))
